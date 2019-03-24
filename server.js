b904f15cf3b71446bcdc5a13ddfa6fdb6a798712
kbpsystem777 <koleen.bp@outlook.com>
2019-03-25 00:35:18 +0800

    welcome to mailserver

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const nodeMailer = require('nodeMailer')
+const nodeMailer = require('nodemailer')
 
 require('dotenv').config()
 
 const port = process.env.PORT || 1964
 
+app.use(bodyParser, urlencoded())
+app.get('/', (req, res) => res.send('Welcome to MCAP Mail Service!'))
 
+app.listen(port, () => console.log(`Server Running on http://localhost:${port}`))
